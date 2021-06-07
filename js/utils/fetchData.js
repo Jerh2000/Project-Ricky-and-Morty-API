b113d253abbdbc59c80a87f8b73d9164e40a3e88
Jairo Rojas <jairo142014@hotmail.com>
2021-06-06 23:51:08 -0500

    fetch data with API

--- js/utils/fetchData.js
+++ js/utils/fetchData.js
@@ -1,19 +1,14 @@
-let XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
-
-const fetchData = (url_api) =>{
-
-    return new Promise((resolve,reject)=>{
-        const xhttp = new XMLHttpRequest();
-        xhttp.open('GET',url_api,true);
-        xhttp.onreadystatechange = (() =>{
-            if(xhttp.readyState === 4){
-                (xhttp.status === 200)
-                ? resolve(JSON.parse(xhttp.responseText))
-                :reject(new Error('Error ', url_api))
-            }   
-        });
-        xhttp.send();
+const fetchData = (api_url) => {
+    return new Promise((resolve, reject) => {
+      const xhttp = new XMLHttpRequest();
+      xhttp.open('GET', api_url, true);
+      xhttp.onreadystatechange = () => {
+        if (xhttp.readyState === 4) {
+          (xhttp.status === 200)
+            ? resolve(JSON.parse(xhttp.responseText))
+            : reject(new Error('Test Error', api_url))
+        }
+      }
+      xhttp.send();
     });
-}
-
-module.exports = fetchData;
+  }
